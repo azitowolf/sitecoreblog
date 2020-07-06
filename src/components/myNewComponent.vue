237b3659241e0303a94681ff0378a01c92500e41
Alexander Zito-Wolf <azitowolf@Alexanders-MacBook-Pro-2.local>
2020-07-06 16:51:57 -0400

    first commit

---
+++ src/components/myNewComponent.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <p>myNewComponent Component</p>
+    <sc-text :field="fields.heading" />
+  </div>
+</template>
+
+<script>
+import { Text } from '@sitecore-jss/sitecore-jss-vue';
+
+export default {
+  name: 'myNewComponent',
+  components: {
+    ScText: Text,
+  },
+  props: {
+    fields: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+}
+</script>
